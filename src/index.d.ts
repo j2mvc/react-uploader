45d11bbd041f72692e5332ccab468e9e32e10af8
杨朔 <wwwdev@qq.com>
2019-08-26 23:31:43 +0800

    write readme.md

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,36 @@
+import * as React from "react";
+import * as types from "./types/app"
+import * as commonTypes from "./types/common"
 
-// export declare  const Upload;
-// export declare  const UploadDialog;
+// 上传组件弹框接受参数
+export type UploadDialogProps = {
+  themeName?:string,// 主题名称
+  locale?: string,// 语种：en|zh默认为zh
+  headers?: any,// 请求头
+  apiUrls?: types.ApiUrls,// 上传组件所用接口地址
+  attachPrefix?: string,// 附件前缀
+  // 以下为上传组件使用参数
+  open: boolean, // 打开弹框true|false
+  success: Function, // 成功返回attach|attaches|url
+  onClose: Function,// 关闭弹框事件
+  type: string, // 文件类型：image|images|file|files,video|videos|audio|audios|flash|flashes
+  attaches?: commonTypes.Attach[], // 预设附件列表
+  url?: string, // 传入预设url
+  fromEditor?: boolean// 从编辑器调用上传器 
+}
+// 上传组件接受参数
+export type UploaderProps = {
+  themeName?:string,// 主题名称
+  locale?: string,// 语种：en|zh默认为zh
+  headers?: any,// 请求头
+  apiUrls?: types.ApiUrls,// 上传组件所用接口地址
+  attachPrefix?: string,// 附件前缀
+  // 以下为上传组件使用参数
+  type: string,// 文件类型：image|images|file|files,video|videos|audio|audios|flash|flashes
+  onChange: Function, // 改变值，返回{attach|attaches|url}
+  cancel?: Function, // 取消动作
+  defaultUrl?: string,// 传入预设url
+  defaultUrls?: string[], // 传入预设urls
+  defaultAttach?: commonTypes.Attach,// 预设附件
+  defaultAttaches?: commonTypes.Attach[]// 预设附件列表
+}
